32e026a730762668cc7f713222e0ce66ae223480
Chris Simmons <chris@turing.edu>
2022-05-11 13:10:19 -0400

    Iteration 1 methods and tests

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,7 +1,22 @@
 class Restaurant
-
+  attr_reader :opening_time, :name, :dishes
+  
   def initialize(opening_time, name)
-
+    @opening_time = opening_time
+    @name = name
+    @dishes = []
   end
+
+  # def opening_time
+  #   @opening_time
+  # end
+  #
+  # def name
+  #   @name
+  # end
+
+  # def dishes
+  #   @dishes
+  # end
 
 end
